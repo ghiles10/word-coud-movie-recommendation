948095858f9d2b83b4255902044029e038c51bad
idris ghiles <ghileshabib@gmail.com>
2022-12-01 18:51:02 +0100

    implementation des fonctions et du type

--- scripts/web_scrap_pokemon.py
+++ scripts/web_scrap_pokemon.py
@@ -3,39 +3,69 @@ import requests
 import re 
 import pandas as pd 
 
-def scrapping_pokemon() : 
-    # recuperation du premier url parent qui contient la liste des pokemon 
-    url = "https://pokemondb.net/pokedex/national"
+def get_links_pokemons() : 
 
-    code_source = requests.get(url)
-    soup = BeautifulSoup(code_source.text, "html.parser") 
+    """retourne une liste qui comporte des liens pour accéder a la page web qui contient les stats de chaque pokemon"""
 
-    #stocker les url's enfants qui contient les stats des pokemon
-    url_2  = [] 
+    # recuperation du premier url parent qui contient la liste des pokemons
+    url_parent = "https://pokemondb.net/pokedex/national"
+    soup = BeautifulSoup(requests.get(url_parent).text, "html.parser") 
+
+    #stocker les url's enfants qui contient les stats de chaque pokemon
+    url_enfants  = [] 
+
     for i in soup.find( 'div' , {'class' : 'infocard-list infocard-list-pkmn-lg'}).findAll("a") : 
         if "ent-name" in str(i) :
-            url_2.append( str("https://pokemondb.net")  + str( i.get("href") ) ) 
+            url_enfants.append( str("https://pokemondb.net")  + str( i.get("href") ) ) 
 
+    return  url_enfants 
 
-    dico_pole_stat = {} # stocker pokemon + statistiques du pokemon
+def get_stats_pokemons(func ) :  
+    
+    """permet de récuperer le nom et les stats des pokemons et retourne un dictionnaire stockant ces données"""
+
+    url = func()
+    poke_statistiques = {} #stocker les stats des pokemons
 
     #recuperation des stats + nom des pokemon
-    for url_poke in url_2  :
+    for url_poke in url  :
         url_poke = BeautifulSoup( requests.get(url_poke).text , "html.parser" ) 
 
         liste_poke_stat = []
 
         for poke_stat in url_poke.find( class_ ='grid-col span-md-12 span-lg-8' ).findAll('td') : 
+
             try : 
-                if isinstance(int(str(poke_stat.getText())), int) :
+                if isinstance(int(str(poke_stat.getText())), int) :        # ne recupérer que le statistiques en gérant les exceptions
                     liste_poke_stat.append(poke_stat.getText())
 
             except ValueError: 
                 pass 
+
         liste_poke_stat =liste_poke_stat[::3 ][:-1]
     
     # pour recuperer nom du pockemon 
         regex_poke = re.findall( r"pokedex\/([a-zA-z]+)" , str(url_poke) )[0]  
-        dico_pole_stat[ str(regex_poke) ] = liste_poke_stat
+        poke_statistiques[ str(regex_poke) ] = liste_poke_stat
 
-    return dico_pole_stat
+    return poke_statistiques
+
+
+def get_type_pokemon() : 
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(len(get_stats_pokemons(get_links_pokemons)))
