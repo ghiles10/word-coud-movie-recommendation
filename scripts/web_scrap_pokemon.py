a12e241cb2a7cead94adc9d23c0524b747ad4819
idris ghiles <ghileshabib@gmail.com>
2022-12-01 21:09:29 +0100

    manque les spécials

--- scripts/web_scrap_pokemon.py
+++ scripts/web_scrap_pokemon.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests 
 import re 
 import pandas as pd 
+import pprint
 
 def get_links_pokemons() : 
 
@@ -20,19 +21,20 @@ def get_links_pokemons() :
 
     return  url_enfants 
 
-def get_stats_pokemons(func ) :  
+def get_stats_and_type_pokemons(func ) :  
     
-    """permet de récuperer le nom et les stats des pokemons et retourne un dictionnaire stockant ces données"""
+    """permet de récuperer le nom, les stats et le type des pokemons et retourne un dictionnaire stockant ces données"""
 
     url = func()
-    poke_statistiques = {} #stocker les stats des pokemons
+    poke_data = {} #stocker les stats des pokemons
 
-    #recuperation des stats + nom des pokemon
+    #recuperation des données 
+    
     for url_poke in url  :
         url_poke = BeautifulSoup( requests.get(url_poke).text , "html.parser" ) 
 
+        #recuperation des stats + nom des pokemon
         liste_poke_stat = []
-
         for poke_stat in url_poke.find( class_ ='grid-col span-md-12 span-lg-8' ).findAll('td') : 
 
             try : 
@@ -43,29 +45,26 @@ def get_stats_pokemons(func ) :
                 pass 
 
         liste_poke_stat =liste_poke_stat[::3 ][:-1]
-    
-    # pour recuperer nom du pockemon 
-        regex_poke = re.findall( r"pokedex\/([a-zA-z]+)" , str(url_poke) )[0]  
-        poke_statistiques[ str(regex_poke) ] = liste_poke_stat
 
-    return poke_statistiques
+    #recuperation des types de pokemon   
+        try : 
+            liste_type_pokemon = []
+            for poke_type in url_poke.find( class_ ='infocard-lg-data text-muted' ).findAll('a') :
+                if "itype" in str(poke_type)   : 
+                    regex_type = re.search(r"itype ([a-zA-Z]+)", str(poke_type)).groups()[0]
+                    liste_type_pokemon.append( regex_type )
+                 
+        except AttributeError:
+            pass
 
-
-def get_type_pokemon() : 
-    
-
-
-
-
-
-
-
+    # pour recuperer nom du pockemon dans le dictionnaire poke stat 
+        regex_poke = re.findall( r"pokedex\/([a-zA-z]+)" , str(url_poke) )[0]
+        poke_data[ str(regex_poke) ] = (liste_poke_stat, liste_type_pokemon) 
 
 
+    return poke_data
 
-
-
+pprint.pprint((get_stats_and_type_pokemons(get_links_pokemons)))
 
 
 
-print(len(get_stats_pokemons(get_links_pokemons)))
