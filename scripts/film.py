9a73a399a5c45a529bd977968c29818b48458bf4
idris ghiles <ghileshabib@gmail.com>
2022-12-03 22:15:31 +0100

    get data filml

--- scripts/film.py
+++ scripts/film.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests 
 import re
-
+import pprint
 
 # récuperer les liens des pages des films 
 def get_url_films() : 
@@ -19,20 +19,47 @@ def get_url_films() :
                 regex_url = re.search(r'href="(.*)"', str(titre)).groups()[0]
                 url_enfants.append('http://allocine.fr'+regex_url)
 
-    return url_enfants
+    return set(url_enfants) 
 
 
 # recuprere les données sur chaque films 
 
-url_films = get_url_films()
-date = []
 
-for film in url_films : 
 
-    soup_film = BeautifulSoup(requests.get(str(film)).text, 'html.parser')   
 
-    for date_html in soup_film.find_all(class_ = 'meta-body-item meta-body-info') : # pour les dates 
-        regex_date =  re.findall(r"[0-9]+ [a-zA-Z]+ [0-9]+", str(date_html))
-        date.append(regex_date)
 
-print(date)
+def get_donnees_film() : 
+
+    url_films = get_url_films()
+    donnees_film = {} 
+
+    for film in url_films : 
+        soup_film = BeautifulSoup(requests.get(str(film)).text, 'html.parser') 
+
+        # recupération des titres 
+        for titre_html in soup_film.find_all(class_ = 'titlebar-title titlebar-title-lg') : 
+            donnees_film[titre_html.text] = []
+
+        for date_html in soup_film.find_all(class_ = 'meta-body-item meta-body-info') :
+
+            # recupération des dates 
+            regex_date =  re.findall(r"[0-9]+ [a-zA-Z]+ [0-9]+", str(date_html))[0]
+            donnees_film[titre_html.text].append(regex_date)
+
+            # récupération de la durée 
+            regex_duree = re.findall(r"[0-9]{1,3}h [0-9]+min", str(date_html))[0]
+            donnees_film[titre_html.text].append(regex_duree)
+
+            # récupération dy type de films 
+            regex_type =  re.findall(r">([a-zA-Z éè]+)<", str(date_html)) 
+            donnees_film[titre_html.text].append(regex_type)
+
+    return donnees_film 
+
+
+
+    
+
+pprint.pprint((donnees_film))
+
+
