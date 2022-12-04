360e386609376a05014932152e7ede5955c6bdfb
idris ghiles <ghileshabib@gmail.com>
2022-12-04 11:33:36 +0100

    ajout note et nb avis

--- scripts/film.py
+++ scripts/film.py
@@ -27,6 +27,7 @@ def get_donnees_film(func) :
 
     url_films = func()
     donnees_film = {} 
+    # avis_film = {}
 
     for film in url_films : 
         soup_film = BeautifulSoup(requests.get(str(film)).text, 'html.parser') 
@@ -35,24 +36,63 @@ def get_donnees_film(func) :
         for titre_html in soup_film.find_all(class_ = 'titlebar-title titlebar-title-lg') : 
             donnees_film[titre_html.text] = []
 
-        for date_html in soup_film.find_all(class_ = 'meta-body-item meta-body-info') :
+        for info_film in soup_film.find_all(class_ = 'meta-body-item meta-body-info') :
 
             # recupération des dates 
-            regex_date =  re.findall(r"[0-9]+ [a-zA-Z]+ [0-9]+", str(date_html))[0]
+            regex_date =  re.findall(r"[0-9]+ [a-zA-Z]+ [0-9]+", str(info_film))[0]
             donnees_film[titre_html.text].append(regex_date)
 
             # récupération de la durée 
-            regex_duree = re.findall(r"[0-9]{1,3}h [0-9]+min", str(date_html))[0]
+            regex_duree = re.findall(r"[0-9]{1,3}h [0-9]+min", str(info_film))[0]
             donnees_film[titre_html.text].append(regex_duree)
 
             # récupération dy type de films 
-            regex_type =  re.findall(r">([a-zA-Z éè]+)<", str(date_html)) 
+            regex_type =  re.findall(r">([a-zA-Z éè]+)<", str(info_film)) 
             donnees_film[titre_html.text].append(regex_type)
 
     return donnees_film 
 
 
 
-pprint.pprint((get_donnees_film(get_url_films)))
+# pprint.pprint((get_donnees_film(get_url_films)))
+    
+
+####récupérer les avis et la note                               
+
+url_films = get_url_films()     
+avis = []                       
+
+liens_avis = []  
+for film in url_films :     
+    soup_film = BeautifulSoup(requests.get(str(film)).content, 'html.parser')   
+
+    # récupération des liens des avis dans la liste liens_avis
+    for page_avis in soup_film.find_all(class_="end-section-link") : 
+    
+        # récupération dy type de films         
+        regex_avis =  re.findall(r'href="(/.*/critiques/spectateurs/)', str(page_avis))  
+
+        if len(regex_avis) > 0 :    
+           lien_avis = "http://allocine.fr" + regex_avis[0]     
+           liens_avis.append(lien_avis)  
+
+# entrer dans les liens des avis                    
+for url_avis in liens_avis : 
+    soup_avis = BeautifulSoup(requests.get(str(url_avis)).content, 'html.parser')   
+
+    # avoir la note
+    for note in soup_avis.find_all(class_  = 'note' ) : 
+        print(note.text) 
+    
+    # nb d'avis
+    for nb_avis in soup_avis.find_all(class_ ="titlebar-title titlebar-title-md") :
+        regex_nb_avis =  re.findall(r'([0-9]+) critiques spectateurs', str(nb_avis))
+        if len(regex_nb_avis) > 0 :
+            print(regex_nb_avis)
+
+    
+
+# print(avis) 
+
 
 
