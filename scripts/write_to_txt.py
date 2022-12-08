0dccf0a1becc01d829f0bc7f4c5c4f89f7f82db2
ghiles idris <96752546+ghiles10@users.noreply.github.com>
2022-12-08 08:44:14 +0000

    fix bug fonction extracion

--- scripts/write_to_txt.py
+++ scripts/write_to_txt.py
@@ -7,8 +7,10 @@ raw_data = film.get_donnees_film()
 def extract_data() : 
     
     for titre, info in raw_data.items() : 
-        with open('data_test.txt', 'a') as f :
-            f.write( str(titre)+ ',' + str(info[0])+',' +str(info[1])+ ',' +str(info[2])+ '\n' )
+        with open('data_film.txt', 'a') as f :
+
+            f.write( str(titre)+ ',' + str(info[0][0])+ ',' + str(info[0][1]) + ',' + str(info[0][2])\
+             +',' + str(info[1][0]) + ',' + str(info[1][1]) + ',' +str(info[2])+ '\n' )
             
 
     f.close()
