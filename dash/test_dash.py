01726e28c85c85e42f31bb6c1f3bb0482e3dfca5
ghiles idris <96752546+ghiles10@users.noreply.github.com>
2023-01-02 00:08:17 +0000

    fix bug dash

--- dash/test_dash.py
+++ dash/test_dash.py
@@ -4,51 +4,50 @@ from dash import html
 import plotly.express as px
 import pandas as pd
 import recuperation_sql_to_pandas
+import random 
 
 # read data
 df = recuperation_sql_to_pandas.sql_to_pandas()
 
 # Import the data for the first graph
-colors = ['rgba(255, 0, 0, {})'.format(i/len(df)) for i in range(len(df))]
-fig = px.histogram(df, x="duree", title="duree", color=colors, histfunc="percent")
+fig1 = px.histogram(df, x="duree", title="Durée film")
 
 
 # Import the data for the second graph
-x2 = df['duree'].value_counts()[0]
-y2 = df['duree']
+df['date'] = pd.to_datetime(df['date'])
+df['month'] = df['date'].dt.month
+fig2 = px.histogram(df, x="month", title="Mois de sortie du film")
 
 # Import the data for the third graph
-x3 = df['duree'].value_counts()[0]
-y3 = df['duree']
+def nettoyage_type(x) : 
+
+    """nettoyage de la colonne type"""
+
+    x['type'] = ' '.join( x['type'].split( )[:2]) 
+    return x 
+
+df = df.apply(nettoyage_type, axis = 1)
+fig3 = px.histogram(df, x="type", title="Type de film")
 
 # Initialize the app
 app = dash.Dash()
 
 # Define the layout of the app
 app.layout = html.Div([
+
     # Add a title
     html.H1('My Web App'),
     # Add a div to hold the graphs
     html.Div([
         
         # Add the first graph (countplot)
-        dcc.Graph(figure=fig),
+        dcc.Graph(figure=fig1),
         # Add the second graph (pie chart)
-        dcc.Graph(
-            id='graph-2',
-            figure={
-                'data': [{'labels': x2, 'values': y2, 'type': 'pie'}],
-                'layout': {'title': 'Second Graph (Pie Chart)'}
-            }
-        ),
+        dcc.Graph(figure=fig2),
+
         # Add the third graph (scatter plot)
-        dcc.Graph(
-            id='graph-3',
-            figure={
-                'data': [{'x': x3, 'y': y3, 'type': 'scatter'}],
-                'layout': {'title': 'Third Graph (Scatter Plot)'}
-            }
-        )
+        dcc.Graph(figure=fig3),
+
     ]),
     # Add some style to make the app look nice
 
