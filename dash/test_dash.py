6e1a39752e3a65044d8a4ec249f53d932dde78be
ghiles idris <96752546+ghiles10@users.noreply.github.com>
2023-01-02 15:26:43 +0000

    affichage word cloud

--- dash/test_dash.py
+++ dash/test_dash.py
@@ -7,16 +7,30 @@ import plotly.io as pio
 import matplotlib.pyplot as plt
 import recuperation_sql_to_pandas
 import word_cloud 
+import base64
+
 
 # read data
 df = recuperation_sql_to_pandas.sql_to_pandas()
 
 
 # fig1 
-fig = px.histogram(df, x="duree", title="duree")
+fig1 = px.histogram(df, x="duree", title="duree")
 
 # Convertir le graphique en code HTML png
 word_cloud.word_cloud()
+
+import os
+# Vérifiez si le fichier "monfichier.txt" existe dans le répertoire courant
+if os.path.exists(r"./data/word_cloud.png"):
+    print("----------------------------------------------------------------------------------->")
+    print("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+else:
+    print("NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN")
+
+image_filename = r"./data/word_cloud.png" # replace with your own image
+encoded_image = base64.b64encode(open(image_filename, 'rb').read())
+
 
 # Import the data for the second graph
 df['date'] = pd.to_datetime(df['date'])
@@ -46,7 +60,7 @@ app.layout = html.Div([
     html.Div([
         
         #wordcloud
-          html.Img(src='word_cloud.png'),
+         html.Img(src='data:image/png;base64,{}'.format(encoded_image.decode())),
         # Add the first graph (countplot)
         dcc.Graph(figure=fig1),
         # Add the second graph (pie chart)
@@ -75,4 +89,4 @@ app.layout = html.Div([
 
 # Run the app
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0')
+    app.run_server(host='0.0.0.0', debug = True)
--- dash/word_cloud.py
+++ dash/word_cloud.py
@@ -15,6 +15,7 @@ def word_cloud() :
     from PIL import Image
 
 
+
     # read data
     df = recuperation_sql_to_pandas.sql_to_pandas()
 
@@ -23,17 +24,22 @@ def word_cloud() :
     # ajout des mots vides
     mots_vides = ["film", "\n"]
     stop_words.update(mots_vides) 
+    print('##############################################################################################')
+    print(stop_words) 
+    print()
+    print()
+    print('##############################################################################################')
+
 
     # Définir le calque du nuage des mots
     wc = WordCloud(background_color="black", max_words=50, stopwords=stop_words, max_font_size=50, random_state=42)
-    plt.figure(figsize= (10,6)) # Initialisation d'une figure
-    wc.generate(df['avis'][0])  # "Calcul" du wordcloud
+    wc.generate(str(df['avis'][0]))  # "Calcul" du wordcloud
     # Obtenir un objet Image à partir de wc
     image = wc.to_image()
 
     # Enregistrer l'image en utilisant la méthode save de l'objet Image
-    image.save('word_cloud.png')
-
+    image.save(r'./data/word_cloud.png')
+    print('########################################################### image enregisterer')
 
 if __name__ =='__main__' : 
     word_cloud() 
